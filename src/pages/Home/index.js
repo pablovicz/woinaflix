5eafc75ab7290b7a5faebe5cbdec9b2d70c3a24b
pablovicz <pablowoina2205@gmail.com>
2020-07-28 13:14:59 -0300

    Commit final dia 2 de imersao

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,9 +11,9 @@ function Home() {
       <Menu />
 
       <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoTitle={dadosIniciais.categorias[3].videos[0].titulo}
+        url={dadosIniciais.categorias[3].videos[0].url}
+        videoDescription={"As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar."}
       />
 
       <Carousel
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -5,8 +5,18 @@ import { Link } from 'react-router-dom';
 function CadastroCategoria() {
     return (
         <PageDefault>
-            <h1>Página de Categoria de Vídeo</h1>
+            <h1>Cadastro de Categoria</h1>
+            <form>
+                <label>
+                    Nome da Categoria:
+                     <input type="text" />
+                </label>
+                <button>
+                    Cadastrar
+                </button>
 
+
+            </form>
             <Link to="/">
                 Ir para home
             </Link>

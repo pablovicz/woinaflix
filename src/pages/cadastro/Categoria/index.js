01618fe89295b195bd3b1b0eaff2b3da97eaf2c3
pablovicz <pablowoina2205@gmail.com>
2020-07-30 21:41:40 -0300

    commit dia 4 imersão alura

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,18 +32,20 @@ function CadastroCategoria() {
     // ============
 
     useEffect(() => {
-        if (window.location.href.includes('localhost')) {
-            const URL = 'http://localhost:8080/categorias';
-            fetch(URL)
-                .then(async (respostaDoServer) => {
-                    if (respostaDoServer.ok) {
-                        const resposta = await respostaDoServer.json();
-                        setCategorias(resposta);
-                        return;
-                    }
-                    throw new Error('Não foi possível pegar os dados');
-                })
-        }
+
+        const URL = window.location.hostname.includes('localhost')
+            ? 'http://localhost:8080/categorias'
+            : 'https://woinaflix.herokuapp.com/categorias';
+        fetch(URL)
+            .then(async (respostaDoServer) => {
+                if (respostaDoServer.ok) {
+                    const resposta = await respostaDoServer.json();
+                    setCategorias(resposta);
+                    return;
+                }
+                throw new Error('Não foi possível pegar os dados');
+            })
+
     }, []);
 
     return (
